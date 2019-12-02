17bfe3ae05f5dbff14df599564fa05e9e780bcbd
gavin stahl <stahlgavin@gmail.com>
2019-12-02 15:37:24 -0800

    finished top part of page and most of middle

--- js/index.js
+++ js/index.js
@@ -40,3 +40,31 @@ const siteContent = {
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
+
+// Nav Links
+let links = document.getElementsByTagName('a');
+links[0].textContent = 'Services';
+links[1].textContent = 'Product';
+links[2].textContent = "Vision";
+links[3].textContent = 'Features';
+links[4].textContent = 'About';
+links[5].textContent = 'Contact';
+
+let h1text = document.querySelector('h1');
+h1text.textContent = "DOM Is Awesome";
+
+let ctabutton = document.querySelector('button');
+ctabutton.textContent = "Get Started";
+
+let bannerImg = document.getElementById("cta-img");
+bannerImg.setAttribute('src', siteContent["cta"]["img-src"]);
+
+let middleImg = document.getElementById("middle-img");
+middleImg.setAttribute('src', siteContent['main-content']["middle-img-src"]);
+
+let mainContent = document.getElementsByClassName('text-content')
+mainContent[0].textContent = "Features";
+mainContent[1].textContent = "About";
+mainContent[2].textContent = "Services";
+mainContent[3].textContent = "Product";
+mainContent[4].textContent = "Vision";
